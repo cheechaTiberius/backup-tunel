d925ec3147a01fc907739c1a67837bb3e479039e
Tibor Laszlo <tibor@laszlo.com.hr>
2024-10-17 19:39:46 +0200

    poletak sql backup skripte

---
+++ Backup-SQL.ps1
@@ -0,0 +1,6 @@
+# konfiguracija
+$BackupPath = "\\Backup\Temp$"
+$Log="BackupLog-$((Get-date).ToString("yyyyMMdd")).log"
+$LogPath = "C:\Scripts\BackupSve\BackupLogs"
+$LocalBackupPath = "E:\OS-Backup\Temp"
+$LocalBackupKomprimiraniPath = "E:\OS-Backup\Komprimirani35"
