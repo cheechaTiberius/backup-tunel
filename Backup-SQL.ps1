ca79729f3885e3ef88c3aec826f4c49417e36e8b
Tibor Laszlo <tibor@laszlo.com.hr>
2024-10-18 13:45:02 +0200

    v1

--- Backup-SQL.ps1
+++ Backup-SQL.ps1
@@ -1,14 +1,23 @@
+# TODO dodati HTML redove u report
+
 # konfiguracija
 $BackupPath = "\\Backup\Temp$"
-$Log="BackupLog-$((Get-date).ToString("yyyyMMdd")).log"
-$LogPath = "C:\Scripts\BackupSve\BackupLogs"
+$Log="SQLBackupLog-$((Get-date).ToString("yyyyMMdd")).log"
+$LogPath = "C:\Scripts\BackupSQL\BackupLogs"
 $LocalBackupPath = "E:\OS-Backup\Temp"
-# $LocalBackupKomprimiraniPath = "E:\OS-Backup\Komprimirani35"
 
 $User = "$(Get-Secret -Name Username -AsPlainText)"
 $Password = "$(Get-Secret -Name Password -AsPlainText)" 
 
-# fukcija za zapisivanje logova
+$MailTable = "<tr> <th> Server </th> <th> Status </th> <th> Napomena </th> </tr>"
+
+$Serveri = @(
+    "TestServer1"
+    "TestServer2"
+)
+
+# ============================================
+
 function Dodaj-log {
     param (
         [Parameter(Mandatory)]
@@ -33,42 +42,89 @@ function Dodaj-log {
     }
     Add-Content -Path "$($LogPath)\$($Log)" $Message   
 }
-# TODO fukncija Pokreni-Backup
 function Pokreni-backup {
     param(
         [Parameter(Mandatory)]
         $Server
     )
-    # TODO obrisi stari backup
-
+    # obrisi stari backup ako postoji i nije root
+    $ServerBackupPath = "$($BackupPath)\WindowsImageBackup\$($Server)"
+    if(($ServerBackupPath -ne "$($BackupPath)\WindowsImageBackup\") -and ($ServerBackupPath)) {
+        Remove-Item -Path $ServerBackupPath -Recurse -Force
+        Dodaj-log "Obrisan $($ServerBackupPath)"
+    }
     # pokreni novi backup
     try {
-        Dodaj-log "Pokrenut backup servera $($server)"
-        Invoke-Command -ComputerName $server -ErrorAction Stop -ScriptBlock {
+        Dodaj-log "Pokrenut backup servera $($Server)"
+        Invoke-Command -ComputerName $Server -ErrorAction Stop -ScriptBlock {
             & 'WBADMIN' @('START', 'BACKUP', "-backuptarget:$($using:BackupPath)", '-include:F:', '-noVerify', '-quiet', "-user:$($using:User)","-password:$($Using:Password)")
         }
     } catch {
-        Dodaj-log "ERROR: Pokretanje backupa nije uspjelo" -Severity Error
+        Dodaj-log "Pokretanje backupa nije uspjelo" -Severity Error
     }
-    # TODO return Get-WBjob statur
+}
+function Provjeri-Backup {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        $Server
+    )
+    try {
+        $output = Invoke-Command -ComputerName $Server -ErrorAction Stop -ScriptBlock {
+            Get-WBJob -Previous 1
+        }
+        Dodaj-log -LogEntry "Status posljednjeg backupa: $($output.JobState)"
+    } catch {
+        Dodaj-log -LogEntry "Nije moguće provjeriti status prethodnog backupa" -Severity Error
+    }
+    # provjeri status nakon zavrsetka
+    if ($output.HResult -ne 0) {
+        # $retry += 'TestServer1'
+        Dodaj-log -LogEntry "Backup $($Server) nije uspio" -Severity Warning
+    } else {
+        Dodaj-log -LogEntry "Backup $($Server) uspio"
+    }
+    return $output.HResult
 }
 
-# TODO inicijalizacija reporta i retry spiska
-$retry = @()
-
-# TODO pokreni backup za prvi server
-$BackupResult = Pokreni-backup -Server TestServer1
-# TODO provjeri status
-if ($BackupResult -ne 0) {
-    # TODO ako backup nije uspio dodaj u retry
-    $retry += 'TestServer1'
+# pokreni prvi backup
+$Serveri | ForEach-Object {
+    Pokreni-backup -Server $_
 }
-    
 
-# TODO pokreni backup za drugi server
+# pokreni provjeru i drugi backup ako je potrebno
+$Serveri | ForEach-Object {
+    $BackupResult = Provjeri-Backup -Server $_
+    if ($BackupResult -eq 0) {
+        Dodaj-log -LogEntry "Backup $($_) uspio"
+        $MailTable += "<tr> <td> $($_) </td> <td> OK </td> <td></td> </tr>"
+    } else {
+        Dodaj-log -LogEntry "Backup $($_) nije uspio" -Severity Warning
+        Pokreni-backup -Server $_
+        Start-Sleep 15
+        $BackupResult = Provjeri-Backup -Server $_
+        if ($BackupResult -eq 0) {
+            Dodaj-log -LogEntry "Ponovljeni backup $($_) uspio"
+            $MailTable += "<tr> <td> $($_) </td> <td> OK </td> <td>(ponovljeno)</td> </tr>"
+        } else {
+            Dodaj-log -LogEntry "Ponovljeni backup $($_) nije uspio" -Severity Error
+            $MailTable += "<tr> <td> $($_) </td> <td> Error </td> </tr>" 
+        }
+    }
+}
 
-# TODO ...
-
-# TODO drugi prolaz, sko je server u retry listi ponovi backup
-
-# TODO pošalji mail report
+# pošalji mail report
+$MailArgs = @{
+    From = "$(Get-Secret -name E-MailUsername -AsPlainText)"
+    To = 'tibor@laszlo.com.hr','dj@meridijana.hr','zeljko.medic@outlook.com'
+    Subject = 'Tunel Ucka SQL Backup'
+    Body = "<HTML><BODY><TABLE> $($MailTable) </TABLE></BODY></HTML>"
+    SmtpServer = "$(Get-Secret -name E-MailServer -AsPlainText)"
+    Credential = New-Object System.Management.Automation.PSCredential ((Get-Secret -name E-MailUsername -AsPlainText),(Get-Secret -name E-MailPassword))
+}
+# $MailArgs
+try {
+    Send-MailMessage @MailArgs -Port 587 -BodyAsHtml -ErrorAction Stop
+} catch {
+    Dodaj-log "Mail obavijest nije poslana: $_" -Severity Warning
+}
